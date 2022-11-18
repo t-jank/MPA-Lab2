bcffa4ab71841349fbc7099bfbe250cd1f837b4a
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-11-18 18:45:31 +0100

    menu done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -10,18 +11,18 @@ public:
     string name;
     int quantity;
     float price;
-    vector < float > history_prices;
+private:
+vector < float > history_prices;
     vector < int > history_quantities;
-
-    void Add(int qu, float pr)
-    {
+public:
+    void Add(int qu, float pr){
         history_prices.push_back(price);
         price = pr;
         quantity += qu;
     }
 
     void Show(){
-        cout << name << "; liczba sztuk w magazynie: " << quantity << "; cena: " << price << endl;
+        cout << name << "; liczba sztuk: " << quantity << "; cena: " << price << endl;
     }
 
     void Show_history(){
@@ -42,7 +43,8 @@ int main()
     int choice=0;
     while(choice!=3){
         //system("cls");
-        cout << "1. Stan magazynu\n"
+        cout << "\n/// Menu glowne ///\n"
+             << "1. Stan magazynu\n"
              << "2. Dodaj produkt\n"
              << "3. Zakoncz\n"
              << "Wybor: ";
@@ -51,9 +53,51 @@ int main()
         switch(choice)
         {
         case 1:
+            cout << "Stan magazynu:\n";
+            for(int i=0; i<magazyn.size(); i++){
+                cout << i+1 << ". ";
+                magazyn[i].Show();
+            }
+            cout << endl;
             break;
-        case 2:
+        case 2:{
+            int i=0;
+            cout << "/// Dodawanie produktu ///\n";
+            for(i=0; i<magazyn.size(); i++){
+                cout << i+1 << ". " << magazyn[i].name << endl;
+            }
+            int product_number = -1, amount=-1; float price=-1;
+            string name;
+            cout << i+1 << ". Nowy produkt\n";
+            cout << i+2 << ". Anuluj\n";
+            cout << "Wybierz produkt: ";
+            cin >> product_number;
+            if(product_number == i+2) {cout<<endl; break;}
+            if(product_number < 1 || product_number > i+1) {cout<<endl; break;}
+
+            if(product_number==i+1){
+                cout << "Podaj nazwe: ";
+                cin >> name;
+                cout << "Podaj ilosc: ";
+                cin >> amount;
+                cout << "Podaj cene: ";
+                cin >> price;
+                Product a;
+                magazyn.push_back(a);
+                magazyn[i].name = name;
+                magazyn[i].quantity = amount;
+                magazyn[i].price = price;
+            }
+            else{
+                cout << "Podaj ilosc: ";
+                cin >> amount;
+                cout << "Podaj cene: ";
+                cin >> price;
+                magazyn[product_number-1].Add(amount,price);
+            }
+            cout << endl;
             break;
+        }
         default:
             break;
         }
